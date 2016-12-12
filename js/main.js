1ef0d5c297c940b5c013723daa6014cbd1dc5be3
jakemalto21 <jakemalto21@gmail.com>
2016-12-12 13:31:34 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var app = angular.module('births', ['nvd3']);
+
+app.controller('births_controller', function($scope, $http) {
+    $scope.options = {
+        chart: {
+            type: 'pieChart',
+            height: 800,
+            x: function(d){
+                return d.province;
+            },
+            y: function(d){
+                return d.millions;
+            },
+            showLabels: true,
+            duration: 500,
+            labelThreshold: 0.01,
+            labelSunbeamLayout: true,
+            legend: {
+                margin: {
+                    top: 5,
+                    right: 35,
+                    bottom: 5,
+                    left: 0
+                }
+            }
+        }
+    };
+    $scope.data = oBirths;
+    
+});
